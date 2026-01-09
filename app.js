e7c55cdb527523ebe087c4f1ea909c9eeb43b2d2
syy <yysun@xreal.com>
2026-01-09 16:25:20 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to CICD Demo App!'));
+app.get('/', (req, res) => res.send(' Now let us test full pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
